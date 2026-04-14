18c6a6b262fdbd3704053a97f75c859f771c234b
Arthur-Gomes-D <arthurgomesdevantier@gmail.com>
2026-04-14 17:20:50 -0300

    nova logica adicionada

--- script/livro.js
+++ script/livro.js
@@ -1,3 +1,10 @@
 function alugarLivro(){
     alert("Livro alugado")
 }
+function estaLogado(){
+    const logado = localStorage.getItem("logado")
+    if(logado == "false"){
+        window.location.href = "usuario.html"
+        
+    }
+}estaLogado()
